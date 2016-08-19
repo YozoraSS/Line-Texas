99459a669f1a146399f79f5c430bbbc0c74eea9b
YozoraSS <h39y21c74@gmail.com>
2016-08-20 01:37:02 +0800

    Update DBhandler.go

--- vendor/github.com/DB/DBhandler.go
+++ vendor/github.com/DB/DBhandler.go
@@ -25,3 +25,49 @@ func UserGamming(MID string) bool{
 
 
 }
+func InRoomInst(MID string){
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	var haveGame string
+	var RID string
+	var R string
+	db.QueryRow("SELECT UserRoom FROM sql6131889.User WHERE MID = ?", content.From).Scan(&R)
+	db.QueryRow("SELECT ID FROM sql6131889.User WHERE  RoomName = ?", R).Scan(&RID)
+	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
+	if haveGame == ""{
+		bot.SendText([]string{MID}, "You can use these instruction:\n!leavechatroom\n!newgame")
+	}else{
+		bot.SendText([]string{MID}, "You can use these instruction:\n!leavechatroom")
+	}
+	db.Close()
+}
+func InRoomNewGame(MID string){
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	var haveGame string
+	var RID string
+	var R string
+	db.QueryRow("SELECT UserRoom FROM sql6131889.User WHERE MID = ?", content.From).Scan(&R)
+	db.QueryRow("SELECT ID FROM sql6131889.Room WHERE  RoomName = ?", R).Scan(&RID)
+	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
+	if haveGame == ""{
+		db.Exec("INSERT INTO sql6131889.Game (GameName, RoomID, GameStatus, GameTokens, GamePlayer1, GameMaster, Cancel) VALUES (?, ?, ?, ?, ?, ?, ?)", "TexasPoker", RID, 100, 0, info[0].MID, "0", 0)
+		bot.SendText([]string{content.From}, "You created a new game")
+	}else{
+		bot.SendText([]string{content.From}, "There is already a game in this room!!")
+	}
+	db.Close()
+}
+func InRoomJoinGame(MID string){
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	var haveGame string
+	var RID string
+	var R string
+	db.QueryRow("SELECT UserRoom FROM sql6131889.User WHERE MID = ?", content.From).Scan(&R)
+	db.QueryRow("SELECT ID FROM sql6131889.Room WHERE  RoomName = ?", R).Scan(&RID)
+	db.QueryRow("SELECT RoomID FROM sql6131889.Game WHERE RoomID = ?", RID).Scan(&haveGame)
+	if haveGame == ""{
+		bot.SendText([]string{content.From}, "Please create a new game")
+	}else{
+		db.Exec("UPDATE sql6131889.Game SET UserStatus = ? WHERE MID = ?", 10, content.From)
+	}
+	db.Close()
+}
