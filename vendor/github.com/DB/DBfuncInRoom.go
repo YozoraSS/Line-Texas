8a54d48a0447cec34135358f39bf8b0b6cef5ead
YozoraSS <h39y21c74@gmail.com>
2016-08-20 05:01:04 +0800

    Update DBfuncInRoom.go

--- vendor/github.com/DB/DBfuncInRoom.go
+++ vendor/github.com/DB/DBfuncInRoom.go
@@ -1,3 +1,7 @@
+/*
+使用者是否在遊戲中
+他手上有哪兩張牌
+*/
 package DB
 import (
 	"os"
@@ -6,9 +10,24 @@ import (
 	"database/sql"
 	_"github.com/go-sql-driver/mysql"
 )
-
+/*
+傳入使用者MID
+回傳使用者是否正在遊戲
+*/
 var bot *linebot.Client
+func UserGamming(MID string) bool{
+	var GameID int
+	GameID = 0;
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	db.QueryRow("SELECT GameID FROM sql6131889.GameAction WHERE MID = ? and Cancel = 0", MID ).Scan(&GameID)
+	if GameID == 0{
+		return false
+	}else{
+		return true
+	}
 
+
+}
 func InRoomInst(MID string){
 	/*strID := os.Getenv("ChannelID")
 	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
@@ -44,7 +63,8 @@ func InRoomNewGame(MID string){
 	if haveGame == ""{
 		db.Exec("INSERT INTO sql6131889.Game (GameName, RoomID, GameStatus, GameTokens, GamePlayer1, GameMaster, Cancel) VALUES (?, ?, ?, ?, ?, ?, ?)", "TexasPoker", RID, 100, 0, MID, "0", 0)
 		
-		db.Exec("INSERT INTO sql6131889.GameAction (MID, GameID, PlayerX, Action, Cancel) VALUE (?, ?, ?, ?, ?)", MID, GID, 20, 0, 0)
+		db.Exec("INSERT INTO sql6131889.GameAction (MID, GameID, PlayerX, Action, Cancel) VALUE (?, ?, ?, ?, ?)", MID, GID, 1, 0, 0)
+		db.Exec("INSERT INTO sql6131889.Game (PlayerNum) VALUE (?)", 1)
 		bot.SendText([]string{MID}, "You created a new game")
 		bot.SendText([]string{MID}, "You are Player1")
 	}else{
@@ -80,40 +100,41 @@ func InRoomJoinGame(MID string){
 		}else{
 			nextPlayer = 50
 		}
-		if nextPlayer <= 29 {
+		if nextPlayer <= 10 {
 			db.Exec("INSERT INTO sql6131889.GameAction (MID, GameID, PlayerX, Action, Cancel) VALUE (?, ?, ?, ?, ?)", MID, GID, nextPlayer, 0, 0)
 			if nextPlayer == 1 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer1 = ? WHERE ID = ?", MID, GID)
-				db.Exec("UPDATE sql6131889.GameAction SET PlayerX = ? WHERE MID = ?", 20, MID)
+				db.Exec("UPDATE sql6131889.GameAction SET PlayerX = ? WHERE MID = ?", 1, MID)
 				bot.SendText([]string{MID}, "You are Player1")
-			}else if nextPlayer == 21 {
+			}else if nextPlayer == 2 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer2 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player2")
-			}else if nextPlayer == 22 {
+			}else if nextPlayer == 3 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer3 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player3")
-			}else if nextPlayer == 23 {
+			}else if nextPlayer == 4 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer4 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player4")
-			}else if nextPlayer == 24 {
+			}else if nextPlayer == 5 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer5 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player5")
-			}else if nextPlayer == 25 {
+			}else if nextPlayer == 6 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer6 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player6")
-			}else if nextPlayer == 26 {
+			}else if nextPlayer == 7 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer7 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player7")
-			}else if nextPlayer == 27 {
+			}else if nextPlayer == 8 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer8 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player8")
-			}else if nextPlayer == 28 {
+			}else if nextPlayer == 9 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer9 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player9")
-			}else if nextPlayer == 29 {
+			}else if nextPlayer == 10 {
 				db.Exec("UPDATE sql6131889.Game SET GamePlayer10 = ? WHERE ID = ?", MID, GID)
 				bot.SendText([]string{MID}, "You are Player10")
 			}
+			db.Exec("INSERT INTO sql6131889.Game (PlayerNum) VALUE (?)", nextPlayer)
 		}else if nextPlayer == 50 {
 			bot.SendText([]string{MID}, "You are already in this game!!")
 		}else{
