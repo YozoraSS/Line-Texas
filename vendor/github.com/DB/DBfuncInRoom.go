63451eb37ddb53f72db126c31b197684d8dc267c
YozoraSS <h39y21c74@gmail.com>
2016-08-20 09:55:19 +0800

    Update DBfuncInRoom.go

--- vendor/github.com/DB/DBfuncInRoom.go
+++ vendor/github.com/DB/DBfuncInRoom.go
@@ -46,7 +46,7 @@ func InRoomNewGame(MID string){
 		db.Exec("INSERT INTO sql6131889.Game (GameName, RoomID, GameStatus, GameTokens, GamePlayer1, GameMaster, PlayerNum, Cancel) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", "TexasPoker", RID, 1, 0, MID, "0", 1, 0)
 		//db.Exec("INSERT INTO sql6131889.GameAction (MID, GameID, PlayerX, Action, Cancel) VALUES (?, ?, ?, ?, ?)", MID, GID, 1, 0, 0)
 		db.Exec("UPDATE sql6131889.Room SET RoomStatus = ? WHERE RoomName = ?", 101, R)
-		DB.InRoomJoinGame(MID)
+		InRoomJoinGame(MID)
 		//bot.SendText([]string{MID}, "You created a new game")
 		//bot.SendText([]string{MID}, "You are Player1")
 	}else{
